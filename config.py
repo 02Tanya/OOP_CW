3b59d69fe2fd664db6b8f3177d503daa86e824c1
Tanya_02 <tr3520@bk.ru>
2024-10-12 02:47:57 +0300

    Fixed bugs with tests

--- config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-root_dir = os.path.dirname(__file__)
-operations_path = os.path.join(root_dir, "src", "operations.json")
--- tests/test_data_processing.py
+++ tests/test_data_processing.py
@@ -3,25 +3,25 @@ import pytest
 from src.data_processing import JSONVacancyData
 
 
-def test_add_vacancies(json_vacancy_data):
+def test_add_data_to_file():
     vacancies = [
         {"title": "Бухгалтер", "salary": 250_000, "city": "Москва"},
         {"title": "Инвестиционный аналитик", "salary": 180_000, "city":
             "Ростов-на-Дону"}
     ]
-    json_vacancy_data.add_data_to_file(vacancies)
-    assert json_vacancy_data.get_data_from_file() == vacancies
+    JSONVacancyData().add_data_to_file(vacancies)
+    assert JSONVacancyData().get_data_from_file() == vacancies
 
 
-def test_remove_data_from_file(json_vacancy_data):
+def test_remove_data_from_file():
     vacancies = [
         {"title": "Бухгалтер", "salary": 250_000, "city": "Москва"},
         {"title": "Инвестиционный аналитик", "salary": 180_000, "city":
             "Ростов-на-Дону"}
     ]
-    json_vacancy_data.add_data_to_file(vacancies)
-    json_vacancy_data.remove_data_from_file()
-    assert os.stat(json_vacancy_data.filename).st_size == 0
+    JSONVacancyData().add_data_to_file(vacancies)
+    JSONVacancyData().remove_data_from_file()
+    assert os.stat(JSONVacancyData().filename).st_size == 0
 
 
 def test_get_data_from_file_empty(tmpdir):
